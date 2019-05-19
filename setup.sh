d6d412debabd3633f2168304754b64878f32508c
okamos <himinato.k@gmail.com>
2019-05-19 22:52:29 +0900

    Fix syntax error

--- setup.sh
+++ setup.sh
@@ -78,23 +78,13 @@ link_files() {
 
 initialize() {
   case ${OSTYPE} in
-    darwin*)
-      run_brew
-
-      if [ ! -e ~/Library/Fonts/Cica-Regular.ttf ]; then
-        wget https://github.com/miiton/Cica/releases/download/v2.1.0/Cica_v2.1.0.zip
-        unar Cica_v2.1.0.zip
-        cp -f Cica_v2.1.0/Cica*.ttf ${HOME}/Library/Fonts/
-        rm -rf Cica_v2.1.0*
-      fi
-      ;;
-    linux-gnu)
-      run_apt
-      ;;
-    *)
+    "darwin*" )
+      run_brew ;;
+    "linux-gnu" )
+      run_apt ;;
+    "*" )
       echo $(tput setaf 1)Working only OSX / Ubuntu!!$(tput sgr0)
-      exit 1
-      ;;
+      exit 1 ;;
   esac
 
   [ ${SHELL} != "/bin/zsh"  ] && chsh -s /bin/zsh
